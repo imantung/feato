f387d7dd2ee4212fd66196ed9f0cb72e5ada5000
iman tung <iman.tung@gmail.com>
2019-05-25 00:35:54 +0700

    add testing for setDefaultIndex

--- feature_test.go
+++ feature_test.go
@@ -11,3 +11,8 @@ func TestFeature_New(t *testing.T) {
 	feature := feato.NewFeature("some-feature")
 	require.Equal(t, feato.DisableIndexToggle, feature.DefaultIndexToggle)
 }
+
+func TestFeature_SetDefaultIndexToggle(t *testing.T) {
+	feature := feato.NewFeature("some-feature").SetDefaultIndexToggle(99)
+	require.Equal(t, feato.IndexToggle(99), feature.DefaultIndexToggle)
+}
